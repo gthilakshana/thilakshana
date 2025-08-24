dac63fcabc77cd5a95590ffcfbfc917acc34ed9b
gthilakshana <gavrawavanniarachchi@gmail.com>
2025-08-24 00:57:03 -0700

    app.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Meta } from "react-head";
 import Home from './pages/homePage'
 import NotFound from './pages/notFoundPage'
 import LoginPage from './pages/loginPage'
 import './App.css'
 
 function App() {
-
-
   return (
     <>
+
+      <Meta
+        name="keywords"
+        content="Gavrawa Thilakshana, gavrawa, thilakshana, Full Stack Developer, React, Node.js, Python, Docker, AWS, Ubuntu, CentOS, Jenkins, Postman, MongoDB, MySQL, Java, C++, Linux, Tailwind, Bootstrap, GitHub"
+      />
 
       <BrowserRouter>
         <div className='w-full h-[100vh]'>
@@ -20,7 +23,6 @@ function App() {
           </Routes>
         </div>
       </BrowserRouter>
-
     </>
   )
 }
